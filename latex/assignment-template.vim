75f2fc98049f47eee6ca9b8c25ccb12047c9749c
Akollek <amiel.kollek@mail.mcgill.ca>
2015-01-27 23:05:18 -0500

    added setup script

--- latex/assignment-template.vim
+++ latex/assignment-template.vim
@@ -4,7 +4,7 @@ if filereadable(filename)
 	echo 'A file of this name already exists'
 	echo 'Please delete to file or choose a new name'
 else
-	execute '!command cat ~/.vim/assignment-template.tex > '.filename
+	execute '!command cat ~/.vim/ak-latex/assignment-template.tex > '.filename
 	e
 	88
 	echo 'Enter assignment number and class'
---
+++ setup-latex.sh
@@ -0,0 +1,11 @@
+mkdir -p ~/.vim/ak-latex
+curl -sS "https://raw.githubusercontent.com/Akollek/vim-scripts/master/latex/latex.vim" > ~/.vim/ak-latex/latex.vim
+curl -sS "https://raw.githubusercontent.com/Akollek/vim-scripts/master/latex/assignment-template.vim" > ~/.vim/ak-latex/assignment-template.vim
+curl -sS "https://raw.githubusercontent.com/Akollek/vim-scripts/master/latex/assignment-template.tex" > ~/.vim/ak-latex/assignment-template.tex 
+
+if [ ! -f ~/.vimrc ]
+	touch ~/.vimrc
+fi
+
+echo "command Latex :source ~/.vim/ak-latex/latex.vim" >> ~/.vimrc
+echo "command Assignment :source ~/.vim/ak-latex/assignment-template.vim" >> ~/.vimrc
